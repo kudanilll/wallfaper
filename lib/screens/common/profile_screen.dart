f60691223b3d7b9dbc03d76434951c87252f5743
kudanill. <achmad24daniel@gmail.com>
2024-03-23 03:19:59 +0700

    add message dialog when user press logout button.

--- lib/screens/common/profile_screen.dart
+++ lib/screens/common/profile_screen.dart
@@ -56,10 +56,26 @@ class _ProfileScreenState extends State<ProfileScreen> {
             child: SizedBox(
               width: double.infinity,
               child: Button(
-                onTap: () {
-                  AuthService().signOut();
-                  Navigator.pushReplacementNamed(context, Routes.login);
-                },
+                onTap: () => showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Logout'),
+                    content: const Text('Are you sure to log out?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          AuthService().signOut();
+                          Navigator.pushReplacementNamed(context, Routes.login);
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                ),
                 text: 'LOG OUT',
               ),
             ),
